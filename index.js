007148650a2a3631954242c3012fe28ddafd2ae1
Anderson Aquino <andersonaquino93@gmail.com>
2022-11-01 00:45:01 -0300

    refactor: add error handling

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.blue('Hello World!'));
+function handleError(error) {
+  throw new Error(chalk.red(error.code, 'Teste'));
+}
+
+async function getFile(filePath) {
+  const encoding = 'utf-8';
+
+  try {
+    const text = await fs.promises.readFile(filePath, encoding);
+    console.log(chalk.green(text));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+getFile('./files/texto.md');
